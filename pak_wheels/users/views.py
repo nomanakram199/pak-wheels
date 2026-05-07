efd6ab63bb20a9c370533f0bbe01576e3cc26629
Noman Akram <noman.akram@cogentlabs.co>
2026-05-07 15:23:47 +0500

    removed unused imports

--- pak_wheels/users/views.py
+++ pak_wheels/users/views.py
@@ -1,5 +1,4 @@
-from rest_framework import generics, status
-from rest_framework.response import Response
+from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import User
 from .serializers import UserRegisterationSerializer, UserSerializer, UserTokenObtainPairSerializer
